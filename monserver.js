1803f61ae5d5f2d611348f9c2be5426b754bc9c3
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-03-05 20:49:32 +0100

    update database port

--- monserver.js
+++ monserver.js
@@ -18,7 +18,7 @@ const pgPool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT || '5432'),
 });
 
 // Vérification de la connexion à PostgreSQL
