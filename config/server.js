c9cea199c525e2eab49750c5b782264355823ad3
Hugo <hugo.chautard@etu.univ-amu.fr>
2025-05-01 15:43:13 +0200

    Correct configuration

--- config/server.js
+++ config/server.js
@@ -13,8 +13,8 @@ dotenv.config();
  * Chargement des certificats SSL
  */
 export const httpsOptions = {
-  key: fs.readFileSync(process.env.SSL_KEY_PATH || "./private-key.pem"),
-  cert: fs.readFileSync(process.env.SSL_CERT_PATH || "./certificate.pem"),
+  key: fs.readFileSync("../../private-key.pem"),
+  cert: fs.readFileSync("../../certificate.pem"),
 };
 
 /**
@@ -22,6 +22,6 @@ export const httpsOptions = {
  */
 export const serverConfig = {
   portHTTPS: process.env.PORT_HTTPS || 3221,
-  frontendURL: process.env.FRONTEND_URL || "https://localhost:3222",
-  staticDir: process.env.STATIC_DIR || './CerisoNet/dist/ceriso-net/browser/'
+  frontendURL: "https://localhost:3222",
+  staticDir: '../../CerisoNet/dist/ceriso-net/browser/'
 };
